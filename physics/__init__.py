835ba97f028f0c69795c5a8cda47fbbe581a4d60
Betnix CEO <gstar3181@gmail.com>
2026-07-23 15:37:08 -0400

    Initialize physics module with joint and emitter imports

---
+++ physics/__init__.py
@@ -0,0 +1,8 @@
+from bemve.physics.joints import SpringJoint, PivotJoint
+from bemve.physics.particle_system import ParticleEmitter
+
+__all__ = [
+    "SpringJoint",
+    "PivotJoint",
+    "ParticleEmitter",
+]
