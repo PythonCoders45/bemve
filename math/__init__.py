6bbe4848def107b6696c48dd099079d878473ee0
Betnix CEO <gstar3181@gmail.com>
2026-07-23 15:42:03 -0400

    Create __init__.py for math module
    Initialize the math module and define public API.

---
+++ math/__init__.py
@@ -0,0 +1,17 @@
+from bemve.math.geometry import CoordinatePlane, VectorField, Arc, Polygon
+from bemve.math.calculus import RiemannSum, DynamicFunction, IntegralRegion
+from bemve.math.linear_algebra import LinearTransform, Matrix2D
+from bemve.math.latex_parser import AdvancedMathTex
+
+__all__ = [
+    "CoordinatePlane",
+    "VectorField",
+    "Arc",
+    "Polygon",
+    "RiemannSum",
+    "DynamicFunction",
+    "IntegralRegion",
+    "LinearTransform",
+    "Matrix2D",
+    "AdvancedMathTex",
+]
